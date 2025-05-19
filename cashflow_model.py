c2ae4854faf1e326788837a86f93faa80b9ac786
susara-ouchithya-M <susara.ouchithya@milliman.com>
2025-05-19 10:54:59 +0530

    initial-commit-0519

---
+++ cashflow_model.py
@@ -0,0 +1,14 @@
+def calculate_cashflow(initial_investment, cashflows, discount_rate):
+    net_present_value = -initial_investment
+    for t, cashflow in enumerate(cashflows, start=1):
+        discounted_cashflow = cashflow / ((1 + discount_rate) ** t)
+        net_present_value += discounted_cashflow
+    return net_present_value
+
+# Example usage
+initial_investment = 1000
+cashflows = [200, 300, 400, 500]
+discount_rate = 0.05
+
+npv = calculate_cashflow(initial_investment, cashflows, discount_rate)
+print(f"Net Present Value: {npv}")
---
+++ test_cashflow_model.py
@@ -0,0 +1,15 @@
+import unittest
+from cashflow_model import calculate_cashflow
+
+class TestCashflowModel(unittest.TestCase):
+    def test_calculate_cashflow(self):
+        initial_investment = 1000
+        cashflows = [200, 300, 400, 500]
+        discount_rate = 0.05
+        expected_npv = 292.58  # Example expected value
+
+        npv = calculate_cashflow(initial_investment, cashflows, discount_rate)
+        self.assertAlmostEqual(npv, expected_npv, places=2)
+
+if __name__ == '__main__':
+    unittest.main()
