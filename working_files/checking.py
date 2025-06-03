fd3f685d297186601cf3d367974b05ccd4953f49
susara-ouchithya-M <susara.ouchithya@milliman.com>
2025-06-03 12:56:47 +0530

    new-ups-6-3-1

--- working_files/checking.py
+++ working_files/checking.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# Checking file
 file_path = '250523_Python_Exercise_Model_Non_Par.xlsx'
 sheet_name_policyinfo = 'Policy Data'
 Policy_info = pd.read_excel(file_path, sheet_name=sheet_name_policyinfo)
